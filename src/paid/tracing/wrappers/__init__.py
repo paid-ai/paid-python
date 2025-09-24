824d429abb3f7f6a93a3c0421ac16dc5384d5175
Ata Altyyev <ata@paid.ai>
2025-09-25 04:52:31 +0500

    export PaidAsyncOpenAI

--- src/paid/tracing/wrappers/__init__.py
+++ src/paid/tracing/wrappers/__init__.py
@@ -4,11 +4,12 @@ from .geminiWrapper import PaidGemini
 from .llamaIndexWrapper import PaidLlamaIndexOpenAI
 from .mistralWrapper import PaidMistral
 from .openaiAgentsWrapper import PaidRunner
-from .openAiWrapper import PaidOpenAI
+from .openAiWrapper import PaidOpenAI, PaidAsyncOpenAI
 from .paidLangChainCallback import PaidLangChainCallback
 
 __all__ = [
     "PaidOpenAI",
+    "PaidAsyncOpenAI",
     "PaidLangChainCallback",
     "PaidMistral",
     "PaidAnthropic",
