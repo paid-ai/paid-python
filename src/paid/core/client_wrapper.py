6f162f2767b94af937c130750035ff07c5590be8
Ata Altyyev <ata@paid.ai>
2026-03-10 17:18:45 +0000

    bump minor version

--- src/paid/core/client_wrapper.py
+++ src/paid/core/client_wrapper.py
@@ -24,12 +24,12 @@ class BaseClientWrapper:
         import platform
 
         headers: typing.Dict[str, str] = {
-            "User-Agent": "paid-python/1.2.1",
+            "User-Agent": "paid-python/1.3.0",
             "X-Fern-Language": "Python",
             "X-Fern-Runtime": f"python/{platform.python_version()}",
             "X-Fern-Platform": f"{platform.system().lower()}/{platform.release()}",
             "X-Fern-SDK-Name": "paid-python",
-            "X-Fern-SDK-Version": "1.2.1",
+            "X-Fern-SDK-Version": "1.3.0",
             **(self.get_custom_headers() or {}),
         }
         headers["Authorization"] = f"Bearer {self._get_token()}"
